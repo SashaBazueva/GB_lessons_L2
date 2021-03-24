ad3fa5eb950eb230821378dc61ceb1cf8c16072b
Sasha <zvezdopad659527@gmail.com>
2021-03-24 23:57:43 +0500

    на пути к решению задания 2(добавила метод add)

--- src/lesson_3/PhoneBook.java
+++ src/lesson_3/PhoneBook.java
@@ -1,5 +1,34 @@
 package lesson_3;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class PhoneBook {
 
+    private HashMap<String, String> phoneBook;
+    private HashSet<String> numbers;
+
+    public PhoneBook() {
+        this.phoneBook = new HashMap<>();
+        this.numbers = new HashSet<>();
+    }
+
+    public void add(String name, String number){
+        if(!numbers.contains(number)){
+            phoneBook.put(number, name);
+            numbers.add(number);
+        }
+    }
+
+    public void printPhoneBook() {
+        for (String keys: numbers) {
+            System.out.print(phoneBook.get(keys) + "=");
+            System.out.print(keys + ", ");
+        }
+    }
+//    public String get(){
+//        return phoneBook.;
+//    }
 }
--- src/lesson_3/Task2.java
+++ src/lesson_3/Task2.java
@@ -9,5 +9,11 @@ package lesson_3;
 public class Task2 {
     public static void main(String[] args) {
 
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Brown", "+7892341");
+        phoneBook.add("Brown", "9033425");
+        phoneBook.add("Kitty", "5464345");
+        phoneBook.printPhoneBook();
+
     }
 }
