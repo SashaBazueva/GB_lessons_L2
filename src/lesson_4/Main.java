63efd974826bc5d0734bd27c6b42bc8bde9c1b45
Sasha <zvezdopad659527@gmail.com>
2021-03-29 21:18:17 +0500

    не знаю как сделать Optional, буду разбираться

--- src/lesson_4/Main.java
+++ src/lesson_4/Main.java
@@ -6,9 +6,10 @@ import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
-        doTask1();
-        doTask2();
-        doTask3();
+//        doTask1();
+//        doTask2();
+//        doTask3();
+        doTask4();
     }
 
     public static void doTask1() {
@@ -17,18 +18,22 @@ public class Main {
         System.out.println();
     }
 
-    public static void doTask2(){
+    public static void doTask2() {
         Set<String> setForTask2 = Set.of("one", "tree", "cat", "mushroom");
         forItem(setForTask2, s -> System.out.println(s));
     }
 
-    public static void doTask3(){
+    public static void doTask3() {
         int number = 10;
         System.out.printf("doubling %d = %d", number, doubleUp(number, () -> 2));
     }
 
-    public static int doubleUp(int integer, Supplier<Integer> supplier){
-        return integer* supplier.get();
+    public static void doTask4() {
+        System.out.println(findAllChars("ccch", 'm'));
+    }
+
+    public static int doubleUp(int integer, Supplier<Integer> supplier) {
+        return integer * supplier.get();
     }
 
     public static void forItem(Set<String> set, Consumer<String> consumer) {
@@ -38,9 +43,20 @@ public class Main {
         }
     }
 
-    public static void findAllChars(String target, char toFind){
+    public static Optional findAllChars(String target, char toFind) {
         char[] ch = target.toCharArray();
-        ArrayList<Character> arrayList = new ArrayList<Character>(Arrays.asList());
-
+        List<Character> chars = new ArrayList<>();
+        String result;
+        for (int i = 0; i < ch.length; i++) {
+            chars.add(ch[i]);
+        }
+        Iterator<Character> iterator = chars.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next() == toFind) {
+                //result = result + toFind;
+            }
+        }
+        if (result.isPresent()) return Optional.of(result);
+        else return Optional.empty();
     }
 }
